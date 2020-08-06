da2668de1191dffe22e6c552eb4c862e8e32ed4f
emanherawy <eman.herawy@gmail.com>
2020-08-06 03:12:04 +0200

    Updates

---
+++ precache-manifest.e996738b9532ef457451155ac96de40a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a9f7e2bd2597fd19eb4f5b0a4a62ed",
+    "url": "/Web3Drive/index.html"
+  },
+  {
+    "revision": "1b2613b55d380e4253d3",
+    "url": "/Web3Drive/static/css/main.b815945f.chunk.css"
+  },
+  {
+    "revision": "29db30f8079faf52a341",
+    "url": "/Web3Drive/static/js/2.77cf88a2.chunk.js"
+  },
+  {
+    "revision": "c8a7d911dfeb1882aeebff51453150ae",
+    "url": "/Web3Drive/static/js/2.77cf88a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ab47ac786245616465",
+    "url": "/Web3Drive/static/js/3.551495b5.chunk.js"
+  },
+  {
+    "revision": "eca47d79d1cfc2d589a880338022bd7c",
+    "url": "/Web3Drive/static/js/3.551495b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0117de1b7d46e4c2a55",
+    "url": "/Web3Drive/static/js/4.56376a8e.chunk.js"
+  },
+  {
+    "revision": "1b2613b55d380e4253d3",
+    "url": "/Web3Drive/static/js/main.9647855b.chunk.js"
+  },
+  {
+    "revision": "ad166503d5e76dd52899",
+    "url": "/Web3Drive/static/js/runtime-main.6fed9cf0.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/Web3Drive/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/Web3Drive/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/Web3Drive/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/Web3Drive/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "ef96b0fd01d526cad7e40ddb5dd2a2c1",
+    "url": "/Web3Drive/static/media/bg1.ef96b0fd.jpg"
+  },
+  {
+    "revision": "456b47bce336ad14b0da2b679a1ea9e7",
+    "url": "/Web3Drive/static/media/bg2.456b47bc.jpg"
+  },
+  {
+    "revision": "bade0f805119c4e72979c6455fec5e1f",
+    "url": "/Web3Drive/static/media/bg3.bade0f80.jpg"
+  },
+  {
+    "revision": "ae3c874d0f8ad41aee2a32dae5fab77b",
+    "url": "/Web3Drive/static/media/bg4.ae3c874d.jpg"
+  },
+  {
+    "revision": "d67305229d9cbfe677b1498d5941d5e0",
+    "url": "/Web3Drive/static/media/bg5.d6730522.jpg"
+  },
+  {
+    "revision": "8ad648fc6fd9ec5fae8804820d767457",
+    "url": "/Web3Drive/static/media/cloud-icn.8ad648fc.png"
+  },
+  {
+    "revision": "2f33824436d60488f58874f6eda67ccf",
+    "url": "/Web3Drive/static/media/preview.2f338244.jpg"
+  },
+  {
+    "revision": "95eef8bb6afe10255f9d2eff806d7e2d",
+    "url": "/Web3Drive/static/media/react.95eef8bb.svg"
+  }
+]);
